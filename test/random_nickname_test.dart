010fa6dfc31f3ad6e5a349922ca63ae610c60960
swimmingkiim <swimmingkiim@gmail.com>
2022-10-10 10:53:36 +0900

    Add test for randomNickname

--- test/random_nickname_test.dart
+++ test/random_nickname_test.dart
@@ -1,16 +1,16 @@
-import 'package:random_nickname/random_nickname.dart';
+// packages
 import 'package:test/test.dart';
+import 'package:random_nickname/random_nickname.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+  group('Test random nickname', () {
+    test('Give two data', () {
+      final nickname = randomNickname([korAdjectiveEmotion, korNounAnimal]);
+      // nickname should not be empty
+      expect(nickname, isNotEmpty);
+      // nickname should be in right order
+      expect(nickname.split(' ')[0], isIn(korAdjectiveEmotion));
+      expect(nickname.split(' ')[1], isIn(korNounAnimal));
     });
   });
 }
